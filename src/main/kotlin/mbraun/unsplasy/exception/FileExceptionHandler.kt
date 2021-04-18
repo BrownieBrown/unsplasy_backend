0fa2cc618b69146ae73849fbae1ed235202651b6
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2021-04-18 20:14:45 +0200

    unified exceptions

---
+++ src/main/kotlin/mbraun/unsplasy/exception/FileExceptionHandler.kt
@@ -0,0 +1,24 @@
+package mbraun.unsplasy.exception
+
+import mbraun.unsplasy.message.ResponseMessage
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.multipart.MaxUploadSizeExceededException
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class FileNotFoundException : ResponseEntityExceptionHandler() {
+
+    @ExceptionHandler(EmptyResultDataAccessException::class)
+    fun handleEmptyResultException(exc: EmptyResultDataAccessException): ResponseEntity<ResponseMessage> {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(ResponseMessage("File does not exist!"))
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException::class)
+    fun handleMaxSizeException(exc: MaxUploadSizeExceededException): ResponseEntity<ResponseMessage> {
+        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(ResponseMessage("File too large!"))
+    }
+}
--- src/main/kotlin/mbraun/unsplasy/exception/FileUploadException.kt
+++
@@ -1,4 +0,0 @@
-package mbraun.unsplasy.exception
-
-class FileUploadException {
-}
